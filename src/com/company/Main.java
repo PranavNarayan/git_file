8092f18e57efb19c11a00d0cf1f0a620351fda69
Pranav Narayan <pranavnarrayan@gmail.com>
2021-05-17 17:40:43 +0530

    init commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("The number is "+ n/2 +" + " + (n-n/2));
+
+    }
+}
