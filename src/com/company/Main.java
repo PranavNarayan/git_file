741126e04c9d27dd80c87e7547cbf19fc5c3467d
Pranav Narayan <pranavnarrayan@gmail.com>
2021-05-17 17:45:14 +0530

    New changes in Main.java file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
+        System.out.println("Hello World :)");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println("The number is "+ n/2 +" + " + (n-n/2));
