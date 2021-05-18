12cac314ce110af459db91b8c10c3f2c83ab5bf4
Pranav Narayan <pranavnarrayan@gmail.com>
2021-05-18 16:22:37 +0530

    changes are made to buy apples

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,11 @@ public class Main {
 	// Takes integer as input the print it
         System.out.println("Hello World :)");
         Scanner sc = new Scanner(System.in);
+        System.out.println("write a amount and rate of apple");
         int n = sc.nextInt();
+        int r = sc.nextInt();
+		apple a = new apple(r);
+		System.out.println("total price of apple you have to pay "+ a.amount(n));
         System.out.println("The number is "+ n/2 +" + " + (n-n/2));
 
     }
