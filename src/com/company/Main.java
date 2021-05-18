14958cab281cc1f8992d709a219d5d7942cd5d5d
Pranav Narayan <pranavnarrayan@gmail.com>
2021-05-18 14:14:38 +0530

    Added SSH

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+	// Takes integer as input the print it
         System.out.println("Hello World :)");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
