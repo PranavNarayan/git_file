31fc3a43ee8c424ddb0ba2241f3a27ae40702508
Pranav Narayan <pranavnarrayan@gmail.com>
2021-05-18 16:27:25 +0530

    added file to calculate the price of apple

---
+++ src/com/company/apple.java
@@ -0,0 +1,10 @@
+package com.company;
+public class apple {
+    private int rate;
+    public apple(int rate){
+        this.rate=rate;
+    }
+    public int amount(int num ){
+        return num*rate;
+    }
+}
